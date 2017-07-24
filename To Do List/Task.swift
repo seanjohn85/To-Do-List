098a52e5a9da7c0d1a82a1b22e63dbfda0bd55bf
John <johnkenny1985@gamil.com>
2017-07-24 22:02:46 +0100

    to do list started

---
+++ To Do List/Task.swift
@@ -0,0 +1,12 @@
+//
+//  Task.swift
+//  To Do List
+//
+//  Created by JOHN KENNY on 24/07/2017.
+//  Copyright © 2017 JOHN KENNY. All rights reserved.
+//
+
+class Task{
+    var name : String = ""
+    var important : Bool = false
+}
--- To Do List/ViewController.swift
+++ To Do List/ViewController.swift
@@ -8,12 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
 
+    
+    var tasks = [Task]()
+    @IBOutlet var tab: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tasks = makeTasks()
+        tab.delegate = self
+        tab.dataSource = self
+        
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cel = UITableViewCell()
+        let task = tasks[indexPath.row]
+        if task.important
+        {
+            cel.textLabel?.text = "‼️\(task.name)"
+            
+        }else{
+            cel.textLabel?.text = task.name
+        }
+        
+        return cel
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -21,5 +47,19 @@ class ViewController: UIViewController {
     }
 
 
+    func makeTasks() -> [Task]{
+        let t1 = Task()
+        t1.name = "test"
+        let t2 = Task()
+        t2.name = "test2"
+        return [t1, t2]
+    }
+    
+    
+    
+    @IBAction func add(_ sender: Any) {
+        performSegue(withIdentifier: "add", sender: nil)
+    }
+    
 }
 
